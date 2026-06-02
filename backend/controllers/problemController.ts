4945d3cb5d107acce0988eb55711067e89010326
Navjot Kaur <navjotkaur8838@gmail.com>
2026-06-02 23:01:48 +0530

    feat: paginate problems

--- backend/controllers/problemController.ts
+++ backend/controllers/problemController.ts
@@ -4,20 +4,45 @@ import { ProblemModel } from "../models/problemModel.js";
 import { TopicModel } from "../models/TopicModel.js";
 
 // @desc   get a problem
-// @route  POST /api/topics/:topicId/problems
+// @route  POST /api/topics/:topicId/problems?page=1&limit=10
 // @access public
 const getTopicProblems = asyncHandler( async(req, res) => {
     const { topicId } = req.params;
  
-    const topic = await TopicModel.findById(topicId).populate('problems');
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit; 
 
+    // const topic = await TopicModel.findById(topicId).populate('problems');
+    // console.log('Topic Name')
+
+    const topic = await TopicModel
+    .findById(topicId)
+    .select('problems')
+    
     if(!topic) {
         res.status(404);
         throw new Error('Topic not found');
     }
 
-    //when user logged in add progess
-     res.status(200).json(topic.problems);
+    const total = topic.problems.length;
+
+    const paginatedIds = topic.problems.slice(skip, skip + limit);
+
+    const problems = await ProblemModel
+    .find({ _id: { $in: paginatedIds } })
+    .select('title level leetCodeLink youtubeLink articleLink');
+
+    const totalPages = Math.ceil(total / limit);
+    const hasNextPage = page < totalPages;
+
+     res.status(200).json({
+        problems,
+        page, 
+        limit,
+        totalPages,
+        hasNextPage,
+     });
 })
 
 // @desc   add a problem
